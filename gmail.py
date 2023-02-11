07e2bb2751891b90a5c0580e81ef576761269049
David Patterson-Cole <dpattersoncole@gmail.com>
2023-02-11 15:38:23 -0500

    gmail refactor + non-chain Q&A

--- gmail.py
+++ gmail.py
@@ -15,6 +15,8 @@ import faiss
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
 import pickle
+from bs4 import BeautifulSoup
+import re
 
 
 # If modifying these scopes, delete the file token.json.
@@ -46,53 +48,130 @@ def main():
     try:
         # Call the Gmail API
         service = build('gmail', 'v1', credentials=creds)
-        email_list_response = service.users().messages().list(userId='me', labelIds=['UNREAD'], maxResults=2).execute()
+        email_list_response = service.users().messages().list(userId='me', maxResults=1000, q="after:2022/02/01 before:2022/02/28").execute()
         results = []
         email_list = email_list_response['messages']
-        print(email_list)
+        # print(email_list)
         for email in email_list:
           if email['id']:
             results.append(service.users().messages().get(userId='me', id=email['id']).execute())
+        print(f'{len(results)=}')
+        metadatas = []
+        docs = []
         for item in results:
-          delivered_to = item['payload']['headers'][0]
-          print(type(delivered_to))
-          received_date = item['payload']['headers'][1]
-          body = item['payload']['parts'][0]['body']['data']
-          # Decode the base64 encoded data
-          body = base64.urlsafe_b64decode(body.encode('ASCII'))
-          # Convert the decoded data to a string
-          body = body.decode('utf-8')
-          text_splitter = CharacterTextSplitter(chunk_size=1500, separator="\n")
-          # docs = [body]
-          # metadatas = []
-          # for i, d in enumerate(data):
-          #     splits = text_splitter.split_text(d)
-          #     docs.extend(splits)
-          #     metadatas.extend([{"source": sources[i]}] * len(splits))
+          for header in item['payload']['headers']:
+            if header['name'] == 'Subject':
+              subject = header['value']
+            if header['name'] == 'From':
+              email_from = header['value']
+            if header['name'] == 'Received':
+              received_date = header['value']
+              # print(f'{received_date=}')
+          # print(item['payload'])
 
+          if 'payload' in item and 'parts' in item['payload']:
+            if 'data' in item['payload']['parts'][0]['body']:
+              body = item['payload']['parts'][0]['body']['data']
+            elif 'parts' in item['payload']['parts'][0]['parts'][0]:
+              # print(item['payload']['parts'])
+              # print('here')
+              # print(item['payload']['parts'][0]['parts'])
+              # print('here')
+              # print(item['payload']['parts'][0]['parts'][0]['parts'])
+              # print('here')
+              body = item['payload']['parts'][0]['parts'][0]['parts'][0]['body']['data']
+            else: 
+              body = item['payload']['parts'][0]['parts'][0]['body']['data']
+          else:
+            body = item['payload']['body']['data']
 
-          # Here we create a vector store from the documents and save it to disk.
-          delivered_str = json.dumps(delivered_to)
-          received_str = json.dumps(received_date)
-          metadata = delivered_str + received_str
-          print(body)
-          print(metadata)
+          if len(body) > 20:
+            # Decode the base64 encoded data
+            body = base64.urlsafe_b64decode(body.encode('UTF-8'))
+            # Convert the decoded data to a string
+            body = body.decode('utf-8')
+            # body = body.replace("-","+").replace("_","/")
+            # decoded_data = base64.b64decode(body)
+            # print(f'{decoded_data=}')
+            # '; |, |\*|\n
+            # text_splitter = CharacterTextSplitter(chunk_size=3900, separator=". | , | \n | ; | :")
+            # splits = text_splitter.split_text(body)
+            splits = text_splitter(body)
+            # print(f'{splits=}')
+            new_splits = []
+            sources = []
+            for i, split in enumerate(splits):
+              new_splits.append(subject + '|' + email_from + '|' + json.dumps(received_date) + '|' + split)
+              sources.append({"source": subject+str(i)})
+            docs.extend(new_splits)
+            metadatas.extend(sources)
+        print(f'{len(docs)=}')
+        for doc in docs:
+          # print(f'{doc=}')
+          if len(doc) > 8000:
+            print(doc[0:150])
+        # print(f'{docs[1]=}')
+        # print(f'{docs[2]=}')
+        # print(f'{docs[3]=}')
+        # print(f'{docs[4]=}')
+        # print(f'{docs[5]=}')
+        # print(f'{docs[6]=}')
+        # print(f'{docs[7]=}')
+        # print(f'{docs[8]=}')
+        # print(f'{docs[9]=}')
+        # print(f'{docs[10]=}')                                                        
+        # print('\n'.join(map(str, docs)))
+        # print(docs)
+        # print(metadatas)
 
-          store = FAISS.from_texts(body, OpenAIEmbeddings(), metadatas=metadata)
-          # This creates an index out of the store data structure
-          faiss.write_index(store.index, "docs.index")
-          # Now that it's saved to disk, we can remove the index from memory
-          store.index = None
-          # This keeps the vectors from the store accessible, but this is not related to the index
-          with open("faiss_store.pkl", "wb") as f:
-              pickle.dump(store, f)
+          # store = FAISS.from_texts(docs, OpenAIEmbeddings())
+        # Might need to iterate through docs and filter out any with token length >8000
 
+        store = FAISS.from_texts(docs, OpenAIEmbeddings(), metadatas=metadatas)
+        # This creates an index out of the store data structure
+        faiss.write_index(store.index, "docs.index")
+        # Now that it's saved to disk, we can remove the index from memory
+        store.index = None
+        # This keeps the vectors from the store accessible, but this is not related to the index
+        with open("faiss_store.pkl", "wb") as f:
+            pickle.dump(store, f)
+        print(f'{len(docs)=}')
+        with open("text.pkl", "wb") as f:
+            pickle.dump(docs, f)
+
+        # with open("text.pkl", "rb") as f:
+        #   text_list = pickle.load(f)
+          # print(f'{len(text_list)=}')
+          # print(f'{text_list[2]=}')
 
 
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
         print(f'An error occurred: {error}')
 
+
+def text_splitter(text):
+    # print(text)
+    chunks = []
+    chunk = ""
+    # naive_split = text.split(". | , | \n | ; | :")
+    text = re.sub('\r', '', text)
+    naive_split = re.split(r"[,;:\n]", text)
+    # print(naive_split)
+    # print(len(naive_split))
+    for split in naive_split:
+      # print(split)
+      if len(chunk) < 3900:
+        # print('here')
+        chunk += split
+        # print(chunk)
+      else:
+        # print(chunk)
+        chunks.append(chunk)
+        chunk = ""
+    chunks.append(chunk)
+    # print(chunks)
+    return chunks
 
 if __name__ == '__main__':
     main()
--- ingest.py
+++ ingest.py
@@ -5,30 +5,51 @@ import faiss
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
 import pickle
+from pypdf import PdfReader
+import re
 
 
 # Here we load in the data in the format that Notion exports it in.
 # ps = list(Path("Text/").glob("**/*.txt"))
-ps = list(Path("Calendly/").glob("**/*.csv"))
+# ps = list(Path("Calendly/").glob("**/*.csv"))
+ps = list(Path("Email/").glob("**/*.pdf"))
+
 
 
 data = []
 sources = []
 for p in ps:
-    with open(p) as f:
-        data.append(f.read())
-    sources.append(p)
+    with open(p, 'rb') as f:
+      print(type(f))
+      pdf = PdfReader(f)
+      output = []
+      for page in pdf.pages:
+          text = page.extract_text()
+          # Merge hyphenated words
+          text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
+          # Fix newlines in the middle of sentences
+          text = re.sub(r"(?<!\n\s)\n(?!\s\n)", " ", text.strip())
+          # Remove multiple newlines
+          text = re.sub(r"\n\s*\n", "\n\n", text)
+          data.append(text)
+        # data.append(f.read())
+          sources.append(p)
 
 # Here we split the documents, as needed, into smaller chunks.
 # We do this due to the context limits of the LLMs.
 text_splitter = CharacterTextSplitter(chunk_size=1500, separator="\n")
 docs = []
 metadatas = []
+# print(data)
+# print(len(data))
 for i, d in enumerate(data):
+    print(f'{i=}')
     splits = text_splitter.split_text(d)
     docs.extend(splits)
     metadatas.extend([{"source": sources[i]}] * len(splits))
 
+print(docs)
+print(metadatas)
 
 # Here we create a vector store from the documents and save it to disk.
 store = FAISS.from_texts(docs, OpenAIEmbeddings(), metadatas=metadatas)
