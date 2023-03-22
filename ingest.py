f83930806922c213e87f8fe0f777e407b9a70687
Memento Mori <xingfanxia@gmail.com>
2023-03-22 01:39:47 -0700

    init

--- ingest.py
+++ ingest.py
@@ -8,7 +8,7 @@ import pickle
 
 
 # Here we load in the data in the format that Notion exports it in.
-ps = list(Path("Notion_DB/").glob("**/*.md"))
+ps = list(Path("notion-dump/").glob("**/*.md"))
 
 data = []
 sources = []
