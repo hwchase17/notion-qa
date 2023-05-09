1da80419780a5bf3ddd930b48ac20680e9926d2c
Fizah Khalid <75387073+fizahkhalid@users.noreply.github.com>
2023-05-09 18:20:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -24,6 +24,34 @@ OPENAI_API_KEY = "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"
 st.set_page_config(page_title="Blendle Notion QA Bot", page_icon=":robot:")
 st.header("Blendle Notion QA Bot")
 
+# Here we load in the data in the format that Notion exports it in.
+ps = list(Path("Notion_DB/").glob("**/*.md"))
+
+data = []
+sources = []
+for p in ps:
+    with open(p) as f:
+        data.append(f.read())
+    sources.append(p)
+
+# Here we split the documents, as needed, into smaller chunks.
+# We do this due to the context limits of the LLMs.
+text_splitter = CharacterTextSplitter(chunk_size=1500, separator="\n")
+docs = []
+metadatas = []
+for i, d in enumerate(data):
+    splits = text_splitter.split_text(d)
+    docs.extend(splits)
+    metadatas.extend([{"source": sources[i]}] * len(splits))
+
+
+# Here we create a vector store from the documents and save it to disk.
+store = FAISS.from_texts(docs, OpenAIEmbeddings(), metadatas=metadatas)
+faiss.write_index(store.index, "docs.index")
+store.index = None
+with open("faiss_store.pkl", "wb") as f:
+    pickle.dump(store, f)
+    
 # READ JSON
 with open("information.json", "r") as f:
     data = json.load(f)
@@ -56,8 +84,8 @@ if proj_seletection:
             break
 
     # Load the LangChain.
-    index = faiss.read_index(index_file_name.replace("indexes/",""))
-    with open(faiss_store_name.replace("faiss_store/",""), "rb") as f:
+    index = faiss.read_index("docs.index")
+    with open("faiss_store.pkl", "rb") as f:
         store = pickle.load(f)
 
     #st.write(store)
